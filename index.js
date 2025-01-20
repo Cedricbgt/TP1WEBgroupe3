94019b2ddf4261953e4f86591b94339da8895333
unknown <cedricbagot106@gmail.com>
2025-01-20 13:59:41 +0100

    anim

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("javascript chargé");
+const boutonAjouterPanier1 = document.getElementById('btn1');
+const boutonAjouterPanier2 = document.getElementById('btn2');
+const boutonAjouterPanier3 = document.getElementById('btn3');
+
+const nomBottes1 = document.querySelector('.div1 .nombottes').textContent;
+const nomBottes2 = document.querySelector('.div2 .nombottes').textContent;
+const nomBottes3 = document.querySelector('.div3 .nombottes').textContent;
+
+boutonAjouterPanier1.addEventListener('click', () => {
+    alert(`Article "${nomBottes1}" ajouté au panier`);
+});
+boutonAjouterPanier2.addEventListener('click', () => {
+    alert(`Article "${nomBottes2}" ajouté au panier`);
+});
+boutonAjouterPanier3.addEventListener('click', () => {
+    alert(`Article "${nomBottes3}" ajouté au panier`);
+});
+
